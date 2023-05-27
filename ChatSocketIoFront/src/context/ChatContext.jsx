7049aa9e55cfa4741db5108b8a77410a29bea984
Arthaz1245 <sergiomendozap96@gmail.com>
2023-05-26 19:27:47 -0500

    prueba

--- ChatSocketIoFront/src/context/ChatContext.jsx
+++ ChatSocketIoFront/src/context/ChatContext.jsx
@@ -5,6 +5,7 @@ import {
   deleteRequest,
   getRequest,
   postRequest,
+  postRequestWithImage,
 } from "../utils/services";
 import { io } from "socket.io-client";
 export const ChatContext = createContext();
@@ -170,37 +171,6 @@ export const ChatContextProvider = ({ children, user }) => {
     }
   }, [deletedChat, currentChat]);
 
-  // const sendTextMessage = useCallback(
-  //   async (
-  //     textMessage,
-  //     sender,
-  //     currentChatId,
-  //     setTextMessage,
-  //     image,
-  //     setImage
-  //   ) => {
-  //     if (!textMessage && !image) {
-  //       return console.log("You must type something or send an image");
-  //     }
-  //     const response = await postRequest(
-  //       `${baseUrl}/messages`,
-  //       JSON.stringify({
-  //         chatId: currentChatId,
-  //         senderId: sender._id,
-  //         text: textMessage,
-  //         image: image ? image : null,
-  //       })
-  //     );
-  //     if (response.error) {
-  //       return setSendTextMessageError(response);
-  //     }
-  //     setNewMessage(response);
-  //     setMessages((prev) => [...prev, response]);
-  //     setTextMessage("");
-  //     setImage(null);
-  //   },
-  //   []
-  // );
   const sendTextMessage = useCallback(
     async (
       textMessage,
@@ -213,38 +183,69 @@ export const ChatContextProvider = ({ children, user }) => {
       if (!textMessage && !image) {
         return console.log("You must type something or send an image");
       }
-
-      const formData = new FormData();
-      formData.append("chatId", currentChatId);
-      formData.append("senderId", sender._id);
-      formData.append("text", textMessage);
-      if (image) {
-        formData.append("image", image);
+      const response = await postRequestWithImage(
+        `${baseUrl}/messages`,
+        JSON.stringify({
+          chatId: currentChatId,
+          senderId: sender._id,
+          text: textMessage,
+          image: image ? image : null,
+        })
+      );
+      if (response.error) {
+        return setSendTextMessageError(response);
       }
-
-      try {
-        const response = await axios.post(
-          `https://chatback-olag.onrender.com/messages`,
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
-
-        const newMessage = response.data;
-        setNewMessage(newMessage);
-        setMessages((prev) => [...prev, newMessage]);
-        setTextMessage("");
-        setImage(null);
-      } catch (error) {
-        console.error(error);
-        setSendTextMessageError(error);
-      }
+      setNewMessage(response);
+      setMessages((prev) => [...prev, response]);
+      setTextMessage("");
+      setImage(null);
     },
     []
   );
+  // const sendTextMessage = useCallback(
+  //   async (
+  //     textMessage,
+  //     sender,
+  //     currentChatId,
+  //     setTextMessage,
+  //     image,
+  //     setImage
+  //   ) => {
+  //     if (!textMessage && !image) {
+  //       return console.log("You must type something or send an image");
+  //     }
+
+  //     const formData = new FormData();
+  //     formData.append("chatId", currentChatId);
+  //     formData.append("senderId", sender._id);
+  //     formData.append("text", textMessage);
+  //     if (image) {
+  //       formData.append("image", image);
+  //     }
+
+  //     try {
+  //       const response = await axios.post(
+  //         `https://chatback-olag.onrender.com/messages`,
+  //         formData,
+  //         {
+  //           headers: {
+  //             "Content-Type": "multipart/form-data",
+  //           },
+  //         }
+  //       );
+
+  //       const newMessage = response.data;
+  //       setNewMessage(newMessage);
+  //       setMessages((prev) => [...prev, newMessage]);
+  //       setTextMessage("");
+  //       setImage(null);
+  //     } catch (error) {
+  //       console.error(error);
+  //       setSendTextMessageError(error);
+  //     }
+  //   },
+  //   []
+  // );
 
   const markAllNotificationsAsRead = useCallback((notifications) => {
     const mNotifications = notifications.map((n) => {
--- ChatSocketIoFront/src/utils/services.js
+++ ChatSocketIoFront/src/utils/services.js
@@ -23,6 +23,29 @@ export const postRequest = async (url, body) => {
   }
   return data;
 };
+export const postRequestWithImage = async (url, body) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    body,
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    let message;
+    if (data?.message) {
+      message = data.message;
+    } else {
+      message = data;
+    }
+    return {
+      error: true,
+      message,
+    };
+  }
+  return data;
+};
 export const getRequest = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
